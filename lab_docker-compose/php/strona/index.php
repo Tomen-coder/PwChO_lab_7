7ea8c3a4cc1ff1cf2228b079246261af5f2a2c05
Tomen-coder <72608745+Tomen-coder@users.noreply.github.com>
2020-12-07 19:10:51 +0100

    Add files via upload

---
+++ lab_docker-compose/php/strona/index.php
@@ -0,0 +1,21 @@
+<?php 
+
+$conn = mysqli_connect('baza_danych', 'root','haslo123');
+if (!$conn) {
+   exit('Nie uzyskano polaczenia: '.mysqli_connect_error());
+}
+echo 'Uzyskano polaczenie, proxy dziala!';
+die();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Nie dziala :(</title>
+</head>
+<body>
+   Nie dziala proxy :(
+</body>
+</html>
